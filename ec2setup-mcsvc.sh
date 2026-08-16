210101f8cada832b447f02f4c0b3adfd43ae3d5c
Joey Abruzzo <joey.abruzzo@outlook.com>
2026-08-15 19:33:19 -0700

    Reduced memory limits to avoid triggering OOM killer

--- ec2setup-mcsvc.sh
+++ ec2setup-mcsvc.sh
@@ -4,7 +4,7 @@ umask 002
 echo "Downloading Minecraft Server 26.2..."
 wget https://piston-data.mojang.com/v1/objects/823e2250d24b3ddac457a60c92a6a941943fcd6a/server.jar
 echo "Starting Minecraft Server..."
-java -Xmx1536M -Xms1536M -jar server.jar nogui
+java -Xmx1024M -Xms1024M -jar server.jar nogui
 echo "Accepting the EULA..."
 echo "eula=true" > eula.txt
 echo "Creating FIFO for server management..."
